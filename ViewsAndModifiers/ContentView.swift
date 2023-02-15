d391b6495839c49d8c3c7530f07e5b96e62b4bd3
SeikaHirori <SeikaHirori@outlook.com>
2023-02-15 12:13:07 -0800

    Starting section 1.10

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -11,28 +11,9 @@ struct ContentView: View {
     let helloWorld:Text = Text("hello world :333")
     
     var body: some View {
-        VStack(spacing: 10) {
-            CapsuleText(text:"First")
-                .foregroundColor(.green)
-            CapsuleText(text: "Second")
-                .foregroundColor(.purple)
-        }
+        helloWorld
     }
     
-    struct CapsuleText: View {
-        var text: String
-        
-        var body: some View {
-            Text(text)
-                .font(.largeTitle)
-                .padding()
-                .background(.blue)
-                .clipShape(Capsule())
-                .padding()
-                .background(.pink)
-                .clipShape(Capsule())
-        }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
