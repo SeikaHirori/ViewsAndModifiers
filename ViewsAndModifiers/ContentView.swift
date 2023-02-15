375b9e480d8e102c639144c56c204dbe897675da
SeikaHirori <SeikaHirori@outlook.com>
2023-02-14 18:05:40 -0800

    added red button, but the order is wrong

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -15,8 +15,15 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
                 .padding()
-                .frame(maxWidth: 200, maxHeight: .infinity)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .background(Color.red)
+            Button("A big button") {
+                
+            }
+            // DEMO, but order of code below results in the red background colour not working as visioned
+            .background(Color.red)
+            .frame(width: 200, height: 200)
+
         }
         .padding()
     }
