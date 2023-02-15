1e3032366b91fddfd390e323623935c6de3f0283
SeikaHirori <SeikaHirori@outlook.com>
2023-02-15 11:33:38 -0800

    Refactored and DRY'd into a custom view

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -12,13 +12,16 @@ struct ContentView: View {
     
     var body: some View {
         VStack(spacing: 10) {
-            Text("First")
-                .font(.largeTitle)
-                .padding()
-                .foregroundColor(.white)
-                .background(.blue)
-                .clipShape(Capsule())
-            Text("Second")
+            CapsuleText(text:"First")
+            CapsuleText(text: "Second")
+        }
+    }
+    
+    struct CapsuleText: View {
+        var text: String
+        
+        var body: some View {
+            Text(text)
                 .font(.largeTitle)
                 .padding()
                 .foregroundColor(.white)
