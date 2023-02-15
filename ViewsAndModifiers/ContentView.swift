d86d0e328bd6561bb6afcf63e7722ced095e7344
SeikaHirori <SeikaHirori@outlook.com>
2023-02-15 11:23:06 -0800

    added view that groups | spells_2

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -12,17 +12,25 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-           spells_1
+           spells_2
         }
     }
     
     var spells_1: some View {
         VStack {
+            Text("spells 1 - VStack edition :3")
             Text("Lumos")
             Text("Obliviate")
         }
     }
     
+    var spells_2: some View {
+        Group {
+            Text("spells 2 - Group edition")
+            Text("Lumos")
+            Text("Obliviate")
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
