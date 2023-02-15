fb96d0930fff70f3dcf788021231ac5692340ae8
SeikaHirori <SeikaHirori@outlook.com>
2023-02-15 10:51:12 -0800

    Starting section 1.7

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,34 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var useRedText:Bool = false
 
-//    // This code looks similar, but it destroys the buttons
-//    var body: some View {
-//        if useRedText {
-//            Button("Hello World :3") {
-//                useRedText.toggle()
-//            }
-//            .foregroundColor(.red)
-//        } else {
-//            Button("Hello World :3") {
-//                useRedText.toggle()
-//            }
-//            .foregroundColor(.blue)
-//        }
-//    }
-    
-    // This, ternary operator, is more efficient; Less boilerplate code
     var body: some View {
         Button("Hello world :3") {
-            useRedText.toggle()
-            debugStatusOf_useRedText()
+            
         }
-        .foregroundColor(useRedText ? .red : .blue)
-    }
-
-    func debugStatusOf_useRedText() {
-        print("Status of red text: \(useRedText)")
     }
 }
 
