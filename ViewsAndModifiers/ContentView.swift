c92626118e349121822d57f0fb0733e5d6258f54
SeikaHirori <SeikaHirori@outlook.com>
2023-02-15 11:18:21 -0800

    assigned modifiers to properties

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     let helloWorld:Text = Text("Hello World :333")
+        .foregroundColor(.green)
     let motto1:Text = Text("Draco dromiens")
     let motto2:Text = Text("nunquam titillandus")
     
@@ -18,7 +19,10 @@ struct ContentView: View {
         VStack {
             helloWorld
             motto1
+                .foregroundColor(.red)
             motto2
+                .foregroundColor(.blue)
+            
         }
     }
 }
