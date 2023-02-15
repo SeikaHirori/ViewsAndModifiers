20f087866c2467919ff3d0cff70a0a53d2df1bbd
SeikaHirori <SeikaHirori@outlook.com>
2023-02-15 11:30:47 -0800

    Created two text views, but it needs to be D.R.Y.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -11,11 +11,21 @@ struct ContentView: View {
     let helloWorld:Text = Text("hello world :333")
     
     var body: some View {
-        VStack {
-           helloWorld
+        VStack(spacing: 10) {
+            Text("First")
+                .font(.largeTitle)
+                .padding()
+                .foregroundColor(.white)
+                .background(.blue)
+                .clipShape(Capsule())
+            Text("Second")
+                .font(.largeTitle)
+                .padding()
+                .foregroundColor(.white)
+                .background(.blue)
+                .clipShape(Capsule())
         }
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
