b2e06bc6fe46dd5532d4e54b4f67357cb16d521d
SeikaHirori <SeikaHirori@outlook.com>
2023-02-15 10:33:55 -0800

    Starting section 1.6

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,12 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-//    // This doesn't work
-//    var body: View {
-//        Text("Hello world :3333")
-//    }
-    
-    // This works 
+
     var body: Text {
         Text("Hello world :3")
     }
